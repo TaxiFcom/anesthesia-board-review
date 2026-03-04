9a8ea9d584c9b7a84f92c576a35e33e81744f23e
SA <150842522+TaxiFcom@users.noreply.github.com>
2026-03-04 17:56:18 +0300

    Add data.js

--- data.js
+++ data.js
@@ -1,1 +1,7 @@
-LARGE_FILE_PLACEHOLDER
+// Combined questions data - loaded from parts
+const QUESTIONS_DATA = [].concat(
+  QUESTIONS_PART1,
+  QUESTIONS_PART2,
+  QUESTIONS_PART3,
+  QUESTIONS_PART4
+);
